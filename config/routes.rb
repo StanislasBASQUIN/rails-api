6aecb13e805b3fe8e26c90c3c7620d58ac4b7769
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-11-24 23:15:57 +0100

    Last RSpecs files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   resources :articles
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %w[show]
+  end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/login',
+      sign_out: 'api/logout',
+      registration: 'api/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
